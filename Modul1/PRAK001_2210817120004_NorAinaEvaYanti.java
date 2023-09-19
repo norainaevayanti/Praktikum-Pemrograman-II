d2221d0e46023a5de904baad9a6bd6c7d2600597
norainaevayanti <128441708+norainaevayanti@users.noreply.github.com>
2023-09-19 20:12:35 +0800

    Add files via upload

---
+++ Modul1/PRAK001_2210817120004_NorAinaEvaYanti.java
@@ -0,0 +1,9 @@
+package PraktikumModul1;
+
+public class PRAK001_2210817120004_NorAinaEvaYanti {
+	public static void main (String[] args) {
+		System.out.println("Selamat Pagi, Nor Aina Eva Yanti");
+		System.out.println("Selamat Siang, Nor Aina Eva Yanti");
+		System.out.println("Selamat Malam, Nor Aina Eva Yanti");
+	}
+}
---
+++ Modul1/PRAK004_2210817120004_NorAinaEvaYanti.java
@@ -0,0 +1,9 @@
+package PraktikumModul1;
+
+public class PRAK004_2210817120004_NorAinaEvaYanti {
+	public static void main(String[] args){
+        String nama = "Nor Aina Eva Yanti";
+        int umur = 20;
+        System.out.print("Perkenalkan, Nama Saya " + nama + ", Umur Saya " + umur);
+    }
+}
