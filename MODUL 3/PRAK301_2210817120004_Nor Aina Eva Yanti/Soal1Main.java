d9ba4ef77545082828cf2d7ce27e1c4479e037f3
norainaevayanti <128441708+norainaevayanti@users.noreply.github.com>
2023-11-02 18:48:02 +0700

    Add files via upload

---
+++ MODUL 3/PRAK301_2210817120004_Nor Aina Eva Yanti/Soal1Main.java
@@ -0,0 +1,23 @@
+package MODUL3;
+import java.util.LinkedList;
+import java.util.Scanner;
+public class Soal1Main {
+	public static void main(String[] args) {
+		try (Scanner input = new Scanner(System.in)) {
+			LinkedList<Dadu> daduList = new LinkedList<>();
+			int totalNilai = 0;
+			int jumlahDadu = input.nextInt();
+			for (int i=0; i<jumlahDadu; i++) {
+			    Dadu dadu = new Dadu();
+			    daduList.add(dadu);
+			}
+			int i = 1;
+			for (Dadu dadu : daduList) {
+			    int nilaiDadu = dadu.getNilai();
+			    totalNilai += nilaiDadu;
+			    System.out.println("Dadu ke-"+ i++ +" bernilai " + nilaiDadu);
+			}
+			 System.out.println("Total nilai dadu keseluruhan " + totalNilai);
+		}
+	}
+}
